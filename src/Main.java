9d6bf783a6c7dd3782040eaf5c65f7a8b56c19b2
AminovaAnna <aminovaanna163@gmail.com>
2023-10-14 21:03:08 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        {
+            int clientOS = 0;
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            default:
+                System.out.println("Неверное значение"); //задача1
+        }
+    }
+        System.out.println();
+
+        {
+            int clientOS = 3;
+            int yearProd = 2015;
+            if (clientOS == 0 && yearProd < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else if (clientOS == 0 && yearProd >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else if (clientOS == 1 && yearProd < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else if (clientOS == 1 && yearProd >= 2015) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Введены некорректные данные");   //задача2
+            }
+        }
+        System.out.println();
+
+        {
+            int year = 2003;
+            if (year >= 1584  ) {
+
+            }
+
+        }
+
+
     }
 }
