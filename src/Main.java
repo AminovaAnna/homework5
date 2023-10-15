083678306dfac3bb6b784725bc5473d9de8a4651
AminovaAnna <aminovaanna163@gmail.com>
2023-10-15 16:21:07 +0300

    Домашняя работа выполнена

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,18 @@ public class Main {
     public static void main(String[] args) {
         {
             int clientOS = 0;
-        switch (clientOS) {
-            case 0:
-                System.out.println("Установите версию приложения для iOS по ссылке");
-                break;
-            case 1:
-                System.out.println("Установите версию приложения для Android по ссылке");
-                break;
-            default:
-                System.out.println("Неверное значение"); //задача1
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Введены некорректные данные");
+        } //задача1
         }
-    }
-        System.out.println();
 
+        System.out.println( );
         {
-            int clientOS = 3;
+            int clientOS = 1;
             int yearProd = 2015;
             if (clientOS == 0 && yearProd < 2015) {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
@@ -33,13 +30,59 @@ public class Main {
         System.out.println();
 
         {
-            int year = 2003;
-            if (year >= 1584  ) {
-
-            }
+            int year = 1000;
+            if (year >= 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                System.out.println(year + " год является високосным");
+            } else {
+                System.out.println(year + " год не является високосным");
+            }    //задача3
+            System.out.println( );
 
         }
+        {
+            int deliveryDistance = 200;
+            int days = 1;
+            if (deliveryDistance > 100) {
+                System.out.println("Доставки нет");
+            } else {
+                if (deliveryDistance > 20) {
+                    days++;
+                }
+                if (deliveryDistance > 60) {
+                    days++;
+                }
+                System.out.println("Потребуется дней: " + days);
+            }//задача4
+            System.out.println();
+        }
+        {
+            int monthNumber = 5;
+            switch (monthNumber) {
+                case 12:
+                case 1:
+                case 2:
+                    System.out.println("Зима");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("Весна");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("Лето");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("Осень");
+                    break;
+                default:
+                    System.out.println("Неизвестный месяц");
+            }  //задача5
 
+        }
 
     }
 }
